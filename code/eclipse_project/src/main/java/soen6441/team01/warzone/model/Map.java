ea48a6ea3fbbfea4eced9d9620b65d5b7448a3a3
John Sekeres <johnsek@teksavvy.com>
2021-02-11 16:22:38 -0500

    added 3 map resources and setup an example unit test for Map::editmap command

--- code/eclipse_project/src/main/java/soen6441/team01/warzone/model/Map.java
+++ code/eclipse_project/src/main/java/soen6441/team01/warzone/model/Map.java
@@ -1,6 +1,60 @@
 package soen6441.team01.warzone.model;
 
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Manages Warzone Maps 
+ *
+ */
 public class Map implements IMapModel, IMapModelView
 {
 
+	/**
+	 * Loads a map from an existing “domination” map file, or creates a new map from scratch if the file does not exist.
+	 *  
+	 * @param map_filename the filename of the "domination" map file
+	 * @throws IOException 
+	 */
+	public void editMap(String map_filename) throws IOException
+	{
+		//show_current_directory();
+		//walk(".");
+		
+		List<String> list = Files.readAllLines(new File(map_filename).toPath(), Charset.defaultCharset() );
+		return;
+	}
+	
+	// to delete
+	private void show_current_directory()
+	{
+		System.out.println("Working Directory = " + System.getProperty("user.dir"));
+	}
+
+	// to delete
+    public void walk( String path ) {
+
+        File root = new File( path );
+        File[] list = root.listFiles();
+
+        if (list == null) return;
+
+        for ( File f : list ) {
+            if ( f.isDirectory() ) {
+                walk( f.getAbsolutePath() );
+                System.out.println( "Dir:" + f.getAbsoluteFile() );
+            }
+            else {
+                System.out.println( "File:" + f.getAbsoluteFile() );
+            }
+        }
+    }
+    
+	
 }
--- code/eclipse_project/src/test/java/soen6441/team01/warzone/model/MapTest.java
+++ code/eclipse_project/src/test/java/soen6441/team01/warzone/model/MapTest.java
@@ -2,16 +2,35 @@ package soen6441.team01.warzone.model;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class MapTest
 {
-
+	//private String MAP_DIR = ".\\target\\test-classes\\maps\\";
+	private String MAP_DIR = "./src/test/resources/maps/";
+	
 	@Test
-	public void test()
+	/**
+	 * Test the editmap command in the model Map class
+	 * Simple test to load an existing valid map file
+	 */
+	public void test_editmap_command_1()
 	{
-		//fail("Not yet implemented");
+		Map map = new Map();
+		try
+		{
+			map.editMap(MAP_DIR + "canada/canada.map");
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+			fail("failure loading an existing valid map");
+		}
 		assertTrue(true);
 	}
 
 }
+
+
+
