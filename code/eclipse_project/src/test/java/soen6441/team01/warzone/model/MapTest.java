d5da9a7d3a67b0ed45a8f89505ace1974d228647
John Sekeres <johnsek@teksavvy.com>
2021-02-11 20:12:24 -0500

    fixed an error with one of the unit tests

--- code/eclipse_project/src/test/java/soen6441/team01/warzone/model/MapTest.java
+++ code/eclipse_project/src/test/java/soen6441/team01/warzone/model/MapTest.java
@@ -12,16 +12,16 @@ import org.junit.Test;
  *
  */
 public class MapTest {
-	private String MAP_DIR = "./src/test/resources/maps/";
+	private String d_MAP_DIR = "./src/test/resources/maps/";
 
 	@Test
 	/**
 	 * Test the editmap. Simple test to load an existing valid map file
 	 */
 	public void test_editmap_command_1() {
-		Map map = new Map();
+		Map l_map = new Map();
 		try {
-			map.loadMap(MAP_DIR + "canada/canada.map");
+			l_map.loadMap(d_MAP_DIR + "canada/canada.map");
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail("failure loading an existing valid map");
@@ -34,9 +34,9 @@ public class MapTest {
 	 * Test the loadmap command. Simple test to load an existing valid map file
 	 */
 	public void test_loadmap_command_1() {
-		Map map = new Map();
+		Map l_map = new Map();
 		try {
-			map.loadMap(MAP_DIR + "canada/canada.map");
+			l_map.loadMap(d_MAP_DIR + "canada/canada.map");
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail("failure loading an existing valid map");
@@ -50,13 +50,19 @@ public class MapTest {
 	 * exception.
 	 */
 	public void test_loadmap_command_2() {
-		Map map = new Map();
+		Boolean l_assert_result = true;
+		Map l_map = new Map();
 		try {
-			map.loadMap(MAP_DIR + "canada/quebec.map");
+			l_map.loadMap(d_MAP_DIR + "canada/quebec.map");
+			l_assert_result = false;
 		} catch (Exception e) {
+		}
+		if( !l_assert_result ) {
+			fail("expecting an exception trying to load a non-existing map file");
+		}
+		else {
 			assertTrue(true);
 		}
-		fail("expecting an exception trying to load a non-existing map file");
 	}
 
 }
