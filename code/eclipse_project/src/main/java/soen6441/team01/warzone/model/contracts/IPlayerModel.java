76588a6db23bfaddf49d780e64478310f047267c
Pouyan Zarbafian <pouyan@Pouyans-MacBook-Pro.local>
2021-03-14 22:05:09 -0400

    class Card, and related method in other classes implemented

--- code/eclipse_project/src/main/java/soen6441/team01/warzone/model/contracts/IPlayerModel.java
+++ code/eclipse_project/src/main/java/soen6441/team01/warzone/model/contracts/IPlayerModel.java
@@ -2,6 +2,8 @@ package soen6441.team01.warzone.model.contracts;
 
 import java.util.ArrayList;
 
+import soen6441.team01.warzone.model.Card;
+
 public interface IPlayerModel {
 
 	String getName();
@@ -25,8 +27,12 @@ public interface IPlayerModel {
 	IOrderModel next_order();
 
 	IPlayerModel deepClonePlayer(IMapModel l_map) throws Exception;
-	
+
 	String deploy(String p_country_name, int p_number_of_armies) throws Exception;
-	
+
 	void copyOrders(IPlayerModel p_cloned_player) throws Exception;
+	
+	void addCard(Card l_card);
+	
+	public ArrayList<Card> getCards();
 }
