c28387c993e416494eb39287f3c80832bd1c8534
John Sekeres <johnsek@teksavvy.com>
2021-03-23 18:24:27 -0400

    fixed up unit test references

--- code/eclipse_project/src/test/java/soen6441/team01/warzone/model/ModelTestSuite.java
+++ code/eclipse_project/src/test/java/soen6441/team01/warzone/model/ModelTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 
 @SuiteClasses({ CardTest.class, ContinentTest.class, CountryTest.class, GamePlayTest.class, MapTest.class,
-		ModelTestSuite.class, OrderAdvanceTest.class, OrderBombTest.class, OrderDeployTest.class, PlayerTest.class })
+		OrderAdvanceTest.class, OrderBombTest.class, OrderDeployTest.class, PlayerTest.class })
 
 /**
  * Test Suite for all the classes inside the package
